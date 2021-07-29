350d6ae4915269e2cb174b69ae6d41601218dc27
AarushPen <81658228+AarushPen@users.noreply.github.com>
2021-07-28 19:50:46 -0500

    Add files via upload

---
+++ PRO-C43-Template-main/js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+   fill("white")
+    textSize(15)
+    text("N F I N C O U T", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a specific task !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A B D S T A E A", 400,300)
+    fill("lightblue")
+    text("Hint: Used to store data !!", 400,320)
+}
